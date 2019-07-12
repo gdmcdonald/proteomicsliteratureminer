a52185d539c8fdd9468221cc72b4d4ddbd4b406e
Gordon McDonald <gordon.mcdonald@sydney.edu.au>
2019-07-12 18:24:48 +1000

    rename the fiunctions in line with the package name

--- R/OmixLitMiner-package.R
+++ R/OmixLitMiner-package.R
@@ -1,7 +1,7 @@
 #' OmixLitMiner: A package for automating literatue retrieval from PubMed based on UniProt identifiers and user-defined keyword search..
 #'
 #' The OmixLitMiner package provides functions:
-#' pubMedMiner
+#' omixLitMiner
 #' @source Steffen P, Wu J, Hariharan S, Molloy MP, Schluter H
 #' Bowel Cancer and Biomarker Research, Kolling Institute, The University of Sydney, Australia
 #'
